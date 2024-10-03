7964d7a6a8165ff9031818ca6ccb510bbab3265a
Michail Tsagris <mtsagris@yahoo.gr>
2024-10-03 11:05:42 +0300

    change

--- R/alfa.comp.rf.R
+++ R/alfa.comp.rf.R
@@ -1,5 +1,6 @@
 alfa.comp.rf <- function(xnew = x, y, x, a = seq(-1, 1, by = 0.1), ntrees, nfeatures, minleaf) {
 
+  config <- as.matrix( expand.grid(ntrees = ntrees, nfeatures = nfeatures, minleaf = minleaf) )
   est <- list()
   if ( min(y) == 0 )  a <- a[a > 0]
   la <- length(a)
@@ -9,9 +10,11 @@ alfa.comp.rf <- function(xnew = x, y, x, a = seq(-1, 1, by = 0.1), ntrees, nfeat
 
   for (i in 1:la) {
     ya <- Compositional::alfa(y, a[i])$aff
-    yhat <- MultivariateRandomForest::build_forest_predict(trainX = x, trainY = ya,
-                           n_tree = ntrees, m_feature = nfeatures, min_leaf = minleaf, testX = xnew)
-    est[[ i ]] <- Compositional::alfainv(yhat, a[i])
+    for (j in 1:p) {
+      yhat <- CompositionalRF::comp.rf(xtest, ytrain, xtrain, type = 0,
+                                      ntrees = config[j, 1], nfeatures = config[j, 2], minleaf = config[j, 3])
+      est[[ i ]][[ j ]] <- Compositional::alfainv(yhat, a[i])
+    }
   }
   est
 
