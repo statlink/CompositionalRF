0a85f0477981317ec32036b1b433cdefb80ffa2a
Michail Tsagris <mtsagris@yahoo.gr>
2024-10-03 10:28:46 +0300

    change

---
+++ R/alfa.comp.rf.R
@@ -0,0 +1,22 @@
+alfa.comp.rf <- function(xnew = x, y, x, a = seq(-1, 1, by = 0.1), ntrees, nfeatures, minleaf) {
+
+  est <- list()
+  if ( min(y) == 0 )  a <- a[a > 0]
+  la <- length(a)
+  nk <- length(k)
+  if ( !is.matrix(xnew) )  xnew <- as.matrix(xnew)
+  nu <- dim(xnew)[1]
+  D <- dim(y)[2]
+  names <- paste("alpha", a)
+  est <- sapply(names, function(x) NULL)
+
+  for (i in 1:la) {
+    ya <- Compositional::alfa(y, a[i])$aff
+    yhat <- MultivariateRandomForest::build_forest_predict(trainX = x, trainY = ya,
+                           n_tree = ntrees, m_feature = nfeatures, min_leaf = minleaf, testX = xnew)
+    est[[ i ]] <- Compositional::alfainv(yhat, a[i])
+  }
+  est
+
+}
+
